b63d8c3f3d95432252a64ddb4f91267f693e89a8
giovanne <giovanne@gmx.net>
2023-02-04 16:36:06 +0100

    Update DEVICE_EB3A.h polling (because of pull #43)
    modified because of: https://github.com/mariolukas/Bluetti_ESP32_Bridge/pull/43

--- Bluetti_ESP32/DEVICE_EB3A.h
+++ Bluetti_ESP32/DEVICE_EB3A.h
@@ -93,10 +93,11 @@ static device_field_data_t bluetti_device_command[] = {
 };
 
 static device_field_data_t bluetti_polling_command[] = {
-  {FIELD_UNDEFINED, 0x00, 0x0A, 0x28 ,0 , 0, TYPE_UNDEFINED},
-  {FIELD_UNDEFINED, 0x00, 0x46, 0x15 ,0 , 0, TYPE_UNDEFINED},
-  {FIELD_UNDEFINED, 0x0B, 0xDA, 0x01 ,0 , 0, TYPE_UNDEFINED},
-  {FIELD_UNDEFINED, 0x0B, 0xF4, 0x07 ,0 , 0, TYPE_UNDEFINED}
+  //{FIELD_UNDEFINED, 0x00, 0x0A, 0x28 ,0 , 0, TYPE_UNDEFINED},
+  //{FIELD_UNDEFINED, 0x00, 0x46, 0x15 ,0 , 0, TYPE_UNDEFINED},
+  //{FIELD_UNDEFINED, 0x0B, 0xDA, 0x01 ,0 , 0, TYPE_UNDEFINED},
+  //{FIELD_UNDEFINED, 0x0B, 0xF4, 0x07 ,0 , 0, TYPE_UNDEFINED},
+  {FIELD_UNDEFINED, 0x00, 0x0A, 0x2A ,0 , 0, TYPE_UNDEFINED}
 
 };
 
