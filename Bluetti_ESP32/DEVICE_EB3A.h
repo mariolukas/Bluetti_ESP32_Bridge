230afee39adfb5b7b7a82df6530b6d930bbd142d
dpleiss <123118135+dpleiss@users.noreply.github.com>
2023-02-04 13:39:59 +0100

    one poll command, including ac/dc status

--- Bluetti_ESP32/DEVICE_EB3A.h
+++ Bluetti_ESP32/DEVICE_EB3A.h
@@ -62,9 +62,12 @@ static device_field_data_t bluetti_device_command[] = {
 };
 
 static device_field_data_t bluetti_polling_command[] = {
-  {FIELD_UNDEFINED, 0x00, 0x0A, 0x28 ,0 , 0, TYPE_UNDEFINED},
-  {FIELD_UNDEFINED, 0x00, 0x46, 0x15 ,0 , 0, TYPE_UNDEFINED},
-  {FIELD_UNDEFINED, 0x0B, 0xB9, 0x3D ,0 , 0, TYPE_UNDEFINED}
+  // this one request is sufficient to get all the currently transmitted data
+  // raising length by 2 also reads the state of ac/dc switches 
+  {FIELD_UNDEFINED, 0x00, 0x0A, 0x2A ,0 , 0, TYPE_UNDEFINED}
+  //,
+  //{FIELD_UNDEFINED, 0x00, 0x46, 0x15 ,0 , 0, TYPE_UNDEFINED},
+  //{FIELD_UNDEFINED, 0x0B, 0xB9, 0x3D ,0 , 0, TYPE_UNDEFINED}
 };
 
 #endif
